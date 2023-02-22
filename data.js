8ff6d5bf08f29ae3a2d9d6dd1028a82f40c6b1e0
CreaNum <creanum@bib.umontreal.ca>
2023-02-22 10:19:29 -0500

    init

---
+++ data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cartothque---bibliothque-des-sciences",
+      "name": "Cartothèque - Bibliothèque des sciences",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.3325381829616525,
+        "pitch": 0.2862772376824765,
+        "fov": 1.470127193399725
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.3094939688473488,
+          "pitch": 0.22510204480796148,
+          "title": "Photographies aériennes",
+          "text": "Ceci est la description"
+        },
+        {
+          "yaw": -0.555669375834805,
+          "pitch": 0.25594911715468527,
+          "title": "Cartes",
+          "text": "Ceci est la description"
+        },
+        {
+          "yaw": -0.5427023570750578,
+          "pitch": -0.004391212922254084,
+          "title": "Numériseur grand format",
+          "text": "Ceci est une description<div><br></div>"
+        },
+        {
+          "yaw": -1.9432412676708282,
+          "pitch": 0.14374691939681483,
+          "title": "Atelier de fabrication numérique",
+          "text": "Pour en savoir plus, consultez bib.umontreal.ca/creanum"
+        }
+      ]
+    }
+  ],
+  "name": "bib-sciences",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
